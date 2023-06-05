3998d2d62220bc25652629ca55197d5e64d823f0
Russell Vacanti <rvacanti@gmail.com>
2023-06-05 17:02:05 -0500

    Move app to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,5 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-  Box,
-  Flex,
-  Heading,
-} from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import AbiApp from "@/comp/App";
 
-import { Hero } from "../components/skel/Hero";
-import { Container } from "../components/skel/Container";
-import { Main } from "../components/skel/Main";
-import { DarkModeSwitch } from "../components/skel/DarkModeSwitch";
-import { CTA } from "../components/skel/CTA";
-import { Footer } from "../components/skel/Footer";
-
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <CTA />
-
-    <Flex
-      w="full"
-      justifyContent="center"
-      alignItems="center"
-      textAlign="center"
-    >
-      <Heading fontSize="md" fontFamily="DM Sans">
-        {"contact: hello@abi.lol"}
-      </Heading>
-    </Flex>
-    <DarkModeSwitch />
-  </Container>
-);
+const Index = () => <AbiApp />;
 
 export default Index;
